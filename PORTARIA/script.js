20f6ec377d8506b848512c74dc48d3b4b538ee1f
noahdondacasagrande-glitch <noahdondacasagrande@gmail.com>
2026-05-07 07:55:59 -0300

    Add files via upload

---
+++ PORTARIA/script.js
@@ -0,0 +1,5 @@
+function check(){
+    var wasBorn = parseInt(document.getElementById('birth').value)
+    var res = document.getElementById('result')
+    var clientAge = document.getElementById('Age')
+}
